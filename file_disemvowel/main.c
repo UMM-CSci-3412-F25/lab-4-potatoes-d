828b22ff7dfd857c67b074c3f331ce601167d38d
Blake Thompson <Thom9170@morris.umn.edu>
2025-10-06 00:49:56 -0500

    Changed the way input and output works to work with gitactions

--- file_disemvowel/main.c
+++ file_disemvowel/main.c
@@ -1,41 +1,18 @@
 #include <stdio.h>
 #include <stdlib.h>
-
 #include "file_disemvowel.h"
 
-int main(int argc, char *argv[]) {
-  if (argc < 3) {
-    return 1;
-  }
-  
-  char *line = NULL;
-  char buffer[1024];
-  
-  const char *input_filename = argv[1];
-  const char *output_filename = argv[2];
-  
-  FILE *fptr_in = fopen(input_filename, "r");
-  
-  if (fptr_in == NULL){
-    printf("Error opening file %s\n", input_filename);
-    return 1;
-  }
-  
-  FILE *fptr_out = fopen(output_filename, "w");
-  if(fptr_out == NULL){
-    printf("Error opening file %s\n", output_filename);
-    return 1;
-  }
+int main() {
+    char buffer[1024];
 
-  while (fgets(buffer, sizeof(buffer), fptr_in)) {
-    char *dis = disemvowel(buffer);
-    if (dis) {
-       fprintf(fptr_out, "%s\n", dis);
-       free((void*) dis);
+    // Read from standard input line by line
+    while (fgets(buffer, sizeof(buffer), stdin)) {
+        char *dis = disemvowel(buffer);
+        if (dis) {
+            fputs(dis, stdout);  // write to standard output
+            free(dis);
+        }
     }
-  }
-  fclose(fptr_in);
-  fclose(fptr_out);
-  free((void*) line);
-  return 0;
+
+    return 0;
 }
